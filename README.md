f9a5f2a59656e88b39054ad879ff112ce2445b5a
Arya Hadi <arya.hadi97@gmail.com>
2020-02-23 15:09:02 +0330

    :tada: initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# .confs
+Including customized configs for:
++ zsh
++ tmux
++ vim
+
+## Installation
+Simply run following commands in your terminal:
+``` sh
+$ git clone https://github.com/aryahadii/.confs ~/.confs
+$ sh ~/.confs/install.sh
+```
+
+## License
+MIT
---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# vim
+ln -s -f ~/.confs/vim ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+echo "VIM configured completely."
+
+# tmux
+ln -s -f ~/.confs/tmux/.tmux.conf ~/.tmux.conf
+ln -s -f ~/.confs/tmux/.tmux.conf.local ~/
+echo "tmux configured completely."
+
+# zsh
+ln -s -f ~/.confs/zsh/.zshrc ~/.zshrc
+ln -s -f ~/.confs/zsh/.zshrc.local ~/.zshrc.local
+echo "zsh configured completely."
+
+# i3
+mkdir -p ~/.i3
+ln -s -f ~/.confs/i3/config ~/.i3/config
+mkdir -p ~/.config/i3status
+ln -s -f ~/.confs/i3status/config ~.config/i3status/config
+ln -s -f ~/.confs/X/.Xresources ~.Xresources
+echo "i3 configured completely."
